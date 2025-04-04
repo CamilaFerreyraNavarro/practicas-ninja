f41533f533cf28ce803b5a12d64559c4169f4cbd
CamilaFerreyraNavarro <camilaferreyra.ayelen72@gmail.com>
2025-04-04 13:05:35 +0200

    tratando de organizar todo

--- ninja_quiz/__manifest__.py
+++ ninja_quiz/__manifest__.py
@@ -1,24 +1,13 @@
- {
-
-	'name':'Ninja Quiz',
-        'version':'1.0',
-        'category':'Tools',
-        'summary':'CLon de Kahoot para odoo',
-        'depends':['survey'],
-        'data': [
-
-	    'views/ninja_quiz_views.xml',
-	    'security/ir.model.access.csv',
-	],
-
-	'installable': True,
-	'application': True,
-
-
-
-
-
-
-
-
- }
+{
+    'name': 'Ninja Quiz',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Clon de Kahoot para Odoo',
+    'depends': ['survey'],
+    'data': [
+        'views/ninja_quiz_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
