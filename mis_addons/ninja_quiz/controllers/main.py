ee16736af0311beed7f0bacaa39b17bdf0cf48b4
CamilaFerreyraNavarro <camilaferreyra.ayelen72@gmail.com>
2025-04-23 14:39:45 +0200

    arreglos + vistas + pendiente: cambiar vistas para que encaje mejor

--- mis_addons/ninja_quiz/controllers/main.py
+++ mis_addons/ninja_quiz/controllers/main.py
@@ -1,6 +1,7 @@
 from odoo import http
 from odoo.http import request
 
+
 class NinjaQuizController(http.Controller):
 
     @http.route('/jugar', auth='public', website=True)
@@ -15,10 +16,10 @@ class NinjaQuizController(http.Controller):
         step = int(step)
         player_id = int(player_id)
 
+        # Guardar la respuesta si viene del botón
         if respuesta_id:
             respuesta_id = int(respuesta_id)
             respuesta = request.env['ninja.answer'].sudo().browse(respuesta_id)
-
             pregunta = respuesta.question_id
 
             request.env['ninja.player.answer'].sudo().create({
@@ -27,30 +28,11 @@ class NinjaQuizController(http.Controller):
                 'answer_id': respuesta.id,
             })
 
+        # Si terminó el juego
         if step >= len(preguntas):
             jugador = request.env['ninja.player'].sudo().browse(player_id)
             return request.render('ninja_quiz.jugar_resultado', {
                 'jugador': jugador,
-            })
-
-       pregunta = preguntas[step]
-        return request.render('ninja_quiz.jugar_pregunta', {
-            'quiz': quiz,
-            'pregunta': pregunta,
-            'step': step,
-            'next_step': step + 1,
-            'player_id': player_id,
-        })
-
-    @http.route('/jugar/quiz', auth='public', website=True)
-    def jugar_quiz(self, quiz_id=0, step=0, **kw):
-        quiz = request.env['ninja.quiz'].sudo().browse(int(quiz_id))
-        preguntas = quiz.question_ids.sorted('id')  
-        step = int(step)
-
-        if step >= len(preguntas):
-            return request.render('ninja_quiz.jugar_resultado', {
-                'quiz': quiz,
             })
 
         pregunta = preguntas[step]
@@ -59,6 +41,7 @@ class NinjaQuizController(http.Controller):
             'pregunta': pregunta,
             'step': step,
             'next_step': step + 1,
+            'player_id': player_id,
         })
 
     @http.route('/jugar/empezar', type='http', auth='public', website=True, methods=['POST'])
